4eb2de14a51dd955089c35a2776b7136b634bf22
Noble Millie <noble.millie@gmail.com>
2019-12-16 03:41:40 -0800

    pre multi-layer

--- script.js
+++ script.js
@@ -1,75 +1,99 @@
 // Dimensions
 const margin = { left: 18, right: 78, top: 78, bottom: 78 };
-let areaWidth = 1000,
-	areaHeight = 800;
+
+let areaWidth = (window.innerWidth - margin.left - margin.right - 10) / 1.05;
+let areaHeight = window.innerHeight - margin.top - margin.bottom - 30;
 
 // Data
-let data = [];
-const days = d3.timeDay.range(new Date(2019, 0, 1), new Date(2019, 11, 31));
+let areaData = [];
+let barData = [];
+const calendarDate = d3.timeDay.range(
+	new Date(2019, 0, 1),
+	new Date(2019, 11, 31)
+);
 
 // Scales
-const xScale = d3
+const areaXScale = d3
 	.scaleTime()
-	.domain(d3.extent(days))
+	.domain(d3.extent(calendarDate))
 	.range([0, Math.PI * 2]); // range is the diameter of the circle
 
-const yScale = d3.scaleRadial().domain([-500, 1000]);
+const areaYScale = d3.scaleRadial().domain([-500, 1000]);
 
 // Generators
 const areaGenerator = d3
 	.areaRadial()
-	.angle(d => xScale(d.date))
-	// .innerRadius(d => yScale(d.v0))
-	.innerRadius(400)
-	.outerRadius(d => yScale(d.v1))
+	.angle(d => areaXScale(d.date))
+	.innerRadius(d => areaYScale(d.v0))
+	// .innerRadius(areaYScale(500))
+	.outerRadius(d => areaYScale(d.v1))
 	.curve(d3.curveBasis);
 
 // Elements
-const areaSvg = d3.select('body').append('svg');
-const g = areaSvg.append('g');
+const areaSvg = d3
+	.select('body')
+	.select('#radial-area')
+	.append('svg');
+// .attr('id', 'area-radial');
+const areaG = areaSvg.append('g');
 
-const xAxis = g.append('g').attr('class', 'axis');
+const areaXAxis = areaG.append('g').attr('class', 'area-axis');
 
-const xAxisTicks = xAxis
-	.selectAll('.tick')
-	.data(d3.timeMonth.every(1).range(...d3.extent(days)))
+const areaXAxisTicks = areaXAxis
+	.selectAll('.area-tick')
+	.data(d3.timeMonth.every(1).range(...d3.extent(calendarDate)))
 	.enter()
 	.append('g')
-	.attr('class', 'tick');
+	.attr('class', 'area-tick');
 
-// xAxisTicks
-// 	.append('text')
-// 	.attr('dx', 65)
-// 	.attr('dy', -15)
-// 	.attr('transform', 'rotate(15)');
-// .text(d => `${d3.timeFormat('%b')(d)}.`);
+areaXAxisTicks
+	.append('text')
+	.attr('dx', 90)
+	.attr('dy', -3)
+	.attr('transform', 'rotate(15)')
+	.text(d => `${d3.timeFormat('%b')(d)}.`);
 
-// xAxisTicks.append('line').attr('y2', -10);
+areaXAxisTicks.append('line').attr('y2', 10);
 
-const yAxis = g.append('g').attr('class', 'axis');
+const areaYAxis = areaG.append('g').attr('class', 'area-axis');
 
-const yAxisTicks = yAxis
+const areaYAxisTicks = areaYAxis
 	.selectAll('.tick')
-	.data(yScale.ticks(10).slice(1))
+	.data(areaYScale.ticks(10).slice(1))
 	.enter()
 	.append('g')
-	.attr('class', 'tick')
+	.attr('class', 'area-tick')
 	.style('opacity', 0.3);
 
-const yAxisCircles = yAxisTicks.append('circle');
+const areaYAxisCircles = areaYAxisTicks.append('circle');
 
-const yAxisTextTop = yAxisTicks
+const areaYAxisTextTop = areaYAxisTicks
 	.append('text')
 	.attr('dy', -5)
 	.text(d => d);
 
-const yAxisTextBottom = yAxisTicks
+const areaYAxisTextBottom = areaYAxisTicks
 	.append('text')
 	.attr('dy', 13)
 	.text(d => d);
 
+const label = areaSvg
+	.append('text')
+	.attrs({
+		y: areaHeight - 15,
+		x: areaWidth / 1.05,
+		stroke: 'lavender',
+		'stroke-width': '0.9px',
+		'font-size': '4em',
+		fill: 'violet',
+		'text-anchor': 'end',
+		// 'margin-top': '40vh',
+		opacity: 0.2
+	})
+	.text(currMonth());
+
 // Updater
-const duration = 1750;
+const duration = 2750;
 
 // const obj = { date: Mon Dec 23 2019 00: 00: 00 GMT - 0800(Pacific Standard Time), v1: 43.09999999999995, v0: 6 }
 
@@ -87,32 +111,32 @@ function redraw(resizing) {
 	width = diameter - margin.left - margin.right;
 	height = diameter - margin.top - margin.bottom;
 
-	yScale.range([0, height / 2]);
+	areaYScale.range([0, height / 2]);
 
 	areaSvg
 		.attr('width', width + margin.left + margin.right)
 		.attr('height', height + margin.top + margin.bottom);
 
-	g.attr(
+	areaG.attr(
 		'transform',
 		`translate(${margin.left + width / 2}, ${margin.top + height / 2})`
 	);
 
-	xAxisTicks.attr('transform', (d, i, e) => {
+	areaXAxisTicks.attr('transform', (d, i, e) => {
 		const point = [width / 2, 0];
 		const angle = (i / e.length) * 360;
 		const rotated = geometric.pointRotate(point, 270 + angle);
 		return `translate(${rotated}) rotate(${angle})`;
 	});
 
-	yAxisCircles.attr('r', d => yScale(d));
+	areaYAxisCircles.attr('r', d => areaYScale(d));
 
-	yAxisTextTop.attr('y', d => yScale(d));
+	areaYAxisTextTop.attr('y', d => areaYScale(d));
 
-	yAxisTextBottom.attr('y', d => -yScale(d));
+	areaYAxisTextBottom.attr('y', d => -areaYScale(d));
 
 	// General update pattern for the area, whose data changes
-	const area = g.selectAll('.area').data([data]);
+	const area = g.selectAll('.area').data([areaData]);
 
 	if (resizing) {
 		area.attr('d', areaGenerator);
@@ -134,24 +158,68 @@ function redraw(resizing) {
 		.style('opacity', 1);
 }
 
+function currMonth() {
+	var month;
+	switch (new Date().getMonth()) {
+		case 0:
+			month = 'January';
+			break;
+		case 1:
+			month = 'February';
+			break;
+		case 2:
+			month = 'March';
+			break;
+		case 3:
+			month = 'April';
+			break;
+		case 10:
+			month = 'November';
+			break;
+		case 11:
+			month = 'february';
+			break;
+		default:
+			month = 'Off Season';
+	}
+	return month;
+}
 // Functions for generating random data
 function makeData() {
 	// let v0 = randBetween(0, 5);
 	// v1 = randBetween(10, 30);
 
-	let v0 = 500;
+	let v0 = 0;
 	let v1 = 1000;
 
-	data = days.map(date => {
-		// v1 = Math.min(v1 + random([-1.7, 2]), 50);
-		// v0 = 5;
+	areaData = calendarDate.map((date, i) => {
+		// v1 = Math.min(v1 + random([-1.7, 2]), 0);
+		if (i < 100) {
+			v1 = i * 10;
+		}
+		if (i >= 100 && i < 330) {
+			v1 = 0;
+		}
+		if (i >= 330) {
+			v1 = (i - 330) * 5;
+		}
+
+		// v1 = 900;
+		v0 = 0;
 		// v0 = Math.min(Math.max(v0 + random([-1, 1]), 1), v1 - 5);
+		// const obj = {
+		// 	date,
+		// 	v1,
+		// 	v0
+		// };
 		const obj = {
+			// date :  Fri Dec 27 2019 00:00:00 GMT-0800,
 			date,
 			v1,
 			v0
 		};
-		console.log(`obj*********: `, obj);
+
+		// console.log(`obj*********: `, obj);
 		return obj;
 	});
 }
